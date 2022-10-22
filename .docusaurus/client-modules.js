3da85f0301457b0ce927550ab154784469f3eec4
Louis <33809479+LouisPetrik@users.noreply.github.com>
2022-10-22 16:07:48 +0200

    removed unnecessary stuff

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/louis/Desktop/projekte/codehints/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/louis/Desktop/projekte/codehints/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/louis/Desktop/projekte/codehints/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/louis/Desktop/projekte/codehints/src/css/custom.css'),
-];
