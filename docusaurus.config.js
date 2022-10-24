9a9740de23739432b27f36d4a175acdca4ed012f
LouisPetrik <louispetrik88@gmail.com>
2022-10-25 00:58:09 +0200

    brought in the new domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,126 +6,126 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'CodeHints',
-  tagline: 'Code concepts through cheat sheets',
-  url: 'https://codehints.netlify.app',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
+	title: 'CodeHints',
+	tagline: 'Code concepts through cheat sheets',
+	url: 'https://codehints.io/',
+	baseUrl: '/',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.png',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: 'facebook', // Usually your GitHub org/user name.
+	projectName: 'docusaurus', // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/LouisPetrik/codehints#contributing',
-        },
-        blog: false,
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					routeBasePath: '/',
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: 'https://github.com/LouisPetrik/codehints#contributing',
+				},
+				blog: false,
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      docs: {
-        sidebar: {
-          // makes the categories drop down collapse once another one is opened up
-          autoCollapseCategories: true,
-        },
-      },
-      navbar: {
-        title: 'CodeHints',
-        logo: {
-          alt: 'CodeHints Logo',
-          src: 'img/logo.png',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Cheat Sheets',
-          },
-          //{ to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/LouisPetrik/codehints',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Codehints',
-            items: [
-              {
-                label: 'Cheat sheets',
-                to: '/',
-              },
-            ],
-          },
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			docs: {
+				sidebar: {
+					// makes the categories drop down collapse once another one is opened up
+					autoCollapseCategories: true,
+				},
+			},
+			navbar: {
+				title: 'CodeHints',
+				logo: {
+					alt: 'CodeHints Logo',
+					src: 'img/logo.png',
+				},
+				items: [
+					{
+						type: 'doc',
+						docId: 'intro',
+						position: 'left',
+						label: 'Cheat Sheets',
+					},
+					//{ to: '/blog', label: 'Blog', position: 'left' },
+					{
+						href: 'https://github.com/LouisPetrik/codehints',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Codehints',
+						items: [
+							{
+								label: 'Cheat sheets',
+								to: '/',
+							},
+						],
+					},
 
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'More Cheat sheets',
-                to: 'https://codingcheats.io/b',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/LouisPetrik/codehints',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Louis Petrik`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['haskell', 'typescript'],
-      },
-    }),
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'More Cheat sheets',
+								to: 'https://codingcheats.io/b',
+							},
+							{
+								label: 'GitHub',
+								href: 'https://github.com/LouisPetrik/codehints',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} Louis Petrik`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+				additionalLanguages: ['haskell', 'typescript'],
+			},
+		}),
 
-  themes: [
-    // ... Your other themes.
-    [
-      require.resolve('@easyops-cn/docusaurus-search-local'),
-      {
-        hashed: true,
-        docsRouteBasePath: '/',
-        indexBlog: false,
-        indexPages: false,
-        explicitSearchResultPath: true,
-      },
-    ],
-  ],
+	themes: [
+		// ... Your other themes.
+		[
+			require.resolve('@easyops-cn/docusaurus-search-local'),
+			{
+				hashed: true,
+				docsRouteBasePath: '/',
+				indexBlog: false,
+				indexPages: false,
+				explicitSearchResultPath: true,
+			},
+		],
+	],
 }
 
 module.exports = config
